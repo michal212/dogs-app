d64b77530dde58277f44ef6a5b27dee11c84100f
michal212 <michalfarada1997@gmail.com>
2021-05-25 15:55:50 +0300

    secondCommit

--- src/Components/Features/DogCard.js
+++ src/Components/Features/DogCard.js
@@ -2,9 +2,9 @@
 export default function DogCard(props) {
   const { name, age, kind } = props;
   // const{borderColor}= props;
- const borderColor= `5px solid ${age>4?"green":"yellow"}` 
+ const borderStyle= `5px solid ${age>4?"green":"yellow"}` 
   return (
-    <div className="dogCard" style= {{border:borderColor }}>
+    <div className="dogCard" style= {{border:borderStyle }}>
       <h1> dog:{name} </h1>
       <h2>age:{age}</h2>
       <h2>kind :{kind}</h2>
